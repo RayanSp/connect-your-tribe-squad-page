2b1a0b18d05352d8421edd940a82eeb959c621df
RayanSp <rayan.spall@hva.nl>
2023-02-16 12:23:31 +0100

    used API's

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Importeer express uit de node_modules map
 import express from 'express'
 
-const url = 'https://whois.fdnd.nl/api/v1/member/rayan-spall'
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
 const data = await fetch(url).then((response) => response.json())
 
 console.log(data)
@@ -23,9 +23,16 @@ app.set('views', './views')
 app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get('/', function (req, res) {
-  // res.send('Hello World!')
-  res.render('index', data)
+app.get('/', (request, response) => {
+  console.log(request.query.squad)
+
+  let slug = request.query.squad || 'squad-b-2022'
+  let orderBy = request.query.orderBy || 'name'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+
+  fetchJson(squadUrl).then((data) => {
+    response.render('index', data)
+  })
 })
 
 // Stel het poortnummer in waar express op gaat luisteren
@@ -36,3 +43,14 @@ app.listen(app.get('port'), function () {
   // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
